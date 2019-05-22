859f5eb95ad77438f4942e11c46b36b2814819a4
Carlos Logroño <cjlgibm.com@carlos-ibms-mbp.raleigh.ibm.com>
2019-05-22 16:41:09 -0400

    feedback updates

--- src/containers/Extension/actions.js
+++ src/containers/Extension/actions.js
@@ -1,4 +1,5 @@
 export { fetchNamespaces } from '../../actions/namespaces';
 export { fetchPipeline, fetchPipelines } from '../../actions/pipelines';
+export { fetchSecrets } from '../../actions/secrets';
 export { fetchTask, fetchTasks } from '../../actions/tasks';
 export { fetchServiceAccounts } from '../../actions/serviceAccounts';
